7895a410ac0bc136872a13bbd30ece77fdb6ae0d
Prabhakar Rudra <prabhakarrudra786@gmail.com>
2026-04-23 11:31:36 +0530

    commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearToken, getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://`${process.env.REACT_APP_API_URL}/api/auth/login`/api"
 });
 
 api.interceptors.request.use((config) => {
